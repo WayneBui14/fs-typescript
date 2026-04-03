07e353c69e9b5a01b1ba6ce32654439597a2543f
Wayne Bui <damienbft@gmail.com>
2026-04-04 02:08:17 +0700

    Exercise: 1. Body mass index

---
+++ healthapp/bmiCalculator.ts
@@ -0,0 +1,16 @@
+const bmiCalculator = (height: number, weight: number): string => {
+  const heightInMeters = height / 100;
+  const bmi = weight / (heightInMeters * heightInMeters);
+  switch (true) {
+    case bmi < 18.5:
+      return "Underweight range";
+    case bmi < 25:
+      return "Normal range";
+    case bmi < 30:
+      return "Overweight range";
+    default:
+      return "Obesity range";
+  }
+};
+
+console.log(bmiCalculator(180, 74));
