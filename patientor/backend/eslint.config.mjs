639e71aaf793b2f6661568fc84b72fcfd8037083
Wayne PC <damienbft@gmail.com>
2026-04-16 00:57:50 +0700

    Run checkup

--- patientor/backend/eslint.config.mjs
+++ patientor/backend/eslint.config.mjs
@@ -2,8 +2,10 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import stylistic from "@stylistic/eslint-plugin";
 
-export default tseslint.config({
-  files: ["**/*.ts"],
+export default tseslint.config(
+  { ignores: ["frontend/**", "dist/**", "build/**"] },
+  {
+    files: ["**/*.ts"],
   extends: [
     eslint.configs.recommended,
     ...tseslint.configs.recommendedTypeChecked,
