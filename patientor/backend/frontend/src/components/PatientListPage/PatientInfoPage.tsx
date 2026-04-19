c04580a5992c96fc1aed56547a357732d6d40e28
Wayne Bui <damienbft@gmail.com>
2026-04-20 01:34:46 +0700

    24. Patientor, step 2

---
+++ patientor/backend/frontend/src/components/PatientListPage/PatientInfoPage.tsx
@@ -0,0 +1,45 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import patientService from "../../services/patients";
+import { Patient, Gender } from "../../types";
+import { Typography } from "@mui/material";
+import MaleIcon from "@mui/icons-material/Male";
+import FemaleIcon from "@mui/icons-material/Female";
+import TransgenderIcon from "@mui/icons-material/Transgender";
+
+const PatientInfoPage = () => {
+  const { id } = useParams<{ id: string }>();
+  const [patient, setPatient] = useState<Patient>();
+
+  useEffect(() => {
+    if (id) {
+      void patientService.getOne(id).then((data) => setPatient(data));
+    }
+  }, [id]);
+
+  if (!patient) return null;
+
+  const GenderIcon = () => {
+    switch (patient.gender) {
+      case Gender.Male:
+        return <MaleIcon />;
+      case Gender.Female:
+        return <FemaleIcon />;
+      default:
+        return <TransgenderIcon />;
+    }
+  };
+
+  return (
+    <div style={{ marginTop: "20px" }}>
+      <Typography variant="h5" style={{ fontWeight: "bold" }}>
+        {patient.name} <GenderIcon />
+      </Typography>
+      <p>ssn: {patient.ssn}</p>
+      <p>occupation: {patient.occupation}</p>
+      <p>date of birth: {patient.dateOfBirth}</p>
+    </div>
+  );
+};
+
+export default PatientInfoPage;
